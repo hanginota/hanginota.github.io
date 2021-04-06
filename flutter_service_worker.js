d88a763e06c70701a0a1cb704c0c384c1028f998
Güneş Tan Cebeci <gnstncbc@gmail.com>
2021-04-06 23:37:29 +0300

    ui v2 commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,9 +31,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "84bd924a8fc7226ca1df884b69eb5223",
-"/": "84bd924a8fc7226ca1df884b69eb5223",
-"main.dart.js": "11ec62860bb14c23afeed97329e73e92",
+"index.html": "9902f79cef871a733e8f5aca686266ef",
+"/": "9902f79cef871a733e8f5aca686266ef",
+"main.dart.js": "39efad322eb1f0944bb3ea4ba6d28526",
 "manifest.json": "d42adc58cd32478a8e40423dee7cfc49",
 "version.json": "15a044b4d396090abebdd111052ed42e"
 };
